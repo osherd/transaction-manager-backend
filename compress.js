2305abc97f46be5d0c303a45414cb112c627442d
osherd <osherdemeke@gmail.com>
2021-04-11 00:31:00 +0300

    Add implementation for the compress function

--- compress.js
+++ compress.js
@@ -1,5 +1,27 @@
 const compress = (transactions) => {
-  return transactions;
+  const counterpartyMap = {};
+
+  // building counterpartyMap
+  transactions.forEach((transaction) => {
+    if (transaction.counterparty in counterpartyMap) {
+      counterpartyMap[transaction.counterparty] += transaction.amount;
+    } else {
+      counterpartyMap[transaction.counterparty] = transaction.amount;
+    }
+  });
+
+  // building compressed transactions from the counterpartyMap
+  const compressedTransactions = [];
+  for (const counterpartyKey in counterpartyMap) {
+    const counterpartyAmount = counterpartyMap[counterpartyKey];
+    compressedTransactions.push({
+      tradingParty: "me",
+      counterparty: counterpartyKey,
+      amount: counterpartyAmount,
+    });
+  }
+
+  return compressedTransactions;
 };
 
 module.exports = compress;
